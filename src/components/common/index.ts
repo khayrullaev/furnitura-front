f056e26ccb9559b09328b6a0fe51aafb756829c0
alisher <khayrullaev.ali@gmail.com>
2022-04-10 23:13:11 +0900

    [components] - ProductCard file added

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,5 @@
 import Button from "./Button";
 import IconButton from "./IconButton";
+import ProductCard from "./ProductCard";
 
-export { Button, IconButton };
+export { Button, IconButton, ProductCard };
