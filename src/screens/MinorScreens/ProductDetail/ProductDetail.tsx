828f3b8a063e4aa9bf5e430456152812b74755a5
alisher <khayrullaev.ali@gmail.com>
2022-06-08 23:52:07 +0900

    [components] - productcard...

--- src/screens/MinorScreens/ProductDetail/ProductDetail.tsx
+++ src/screens/MinorScreens/ProductDetail/ProductDetail.tsx
@@ -1,103 +1,84 @@
-import React, { useEffect, useCallback, useMemo, useRef } from "react";
-import { View, Text, Button, StyleSheet, Alert, TextInput } from "react-native";
-import { useLoadingContext } from "../../../hooks";
-import BottomSheet, {
-  BottomSheetBackdrop,
-  BottomSheetModal,
-  BottomSheetModalProvider,
-  BottomSheetView,
-  BottomSheetBackdropProps,
-} from "@gorhom/bottom-sheet";
+import React from "react";
+import { View, StyleSheet, ImageBackground, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import styled from "styled-components/native";
+
+// components
+import { Block, IconButton } from "../../../components/common";
+
+// assets
+import {
+  LeftArrow,
+  ShareIcon,
+  ReportIcon,
+  HeartIcon,
+} from "../../../components/svgicons";
+
+// styles
 import { theme } from "../../../styles";
-import { Easing } from "react-native-reanimated";
 
-const ProductDetail = () => {
-  const { toggleLoading } = useLoadingContext();
-
-  useEffect(() => {
-    toggleLoading(true);
-
-    setTimeout(() => {
-      toggleLoading(false);
-    }, 1500);
-  }, []);
-
-  // ref
-  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
-
-  // variables
-  const snapPoints = useMemo(() => ["25%", "50%", "75%"], []);
-
-  // callbacks
-  const handlePresentModalPress = useCallback(() => {
-    bottomSheetModalRef.current?.present();
-  }, []);
-  const handleSheetChanges = useCallback((index: number) => {
-    console.log("handleSheetChanges", index);
-  }, []);
-  const handlePresentModalClose = useCallback(() => {
-    bottomSheetModalRef.current?.dismiss();
-  }, []);
-
-  // renders
-  const renderBackdrop = useCallback(
-    (props) => (
-      <BottomSheetBackdrop
-        {...props}
-        disappearsOnIndex={-1}
-        appearsOnIndex={1}
-        pressBehavior="close"
-        opacity={0.3}
-      />
-    ),
-    []
-  );
-
+const ProductDetail = ({ navigation, route }: any) => {
   return (
-    <View style={styles.container}>
-      <Button
-        onPress={handlePresentModalPress}
-        title="Present Modal"
-        color="black"
-      />
-      <Button
-        onPress={handlePresentModalClose}
-        title="Close Modal"
-        color="black"
-      />
-      <BottomSheetModal
-        enablePanDownToClose={true}
-        ref={bottomSheetModalRef}
-        index={1}
-        snapPoints={snapPoints}
-        onChange={handleSheetChanges}
-        backdropComponent={renderBackdrop}
-        handleIndicatorStyle={{ backgroundColor: theme.main }}
+    <SafeAreaView style={styles.container}>
+      <Block
+        flexDirection="row"
+        justifyContent="space-between"
+        ph={24}
+        pv={24}
+        style={{ height: "40%" }}
       >
-        <BottomSheetView style={styles.contentContainer}>
-          <Text>Awesome 🎉</Text>
-          <Button
-            onPress={handlePresentModalClose}
-            title="Close Modal"
-            color="black"
+        <IconButton icon={LeftArrow} onPress={() => navigation.goBack()} />
+        <Block justifyContent="center" alignItems="center">
+          <Image
+            style={{ width: 250, height: 250 }}
+            source={{ url: route.params.src }}
           />
-        </BottomSheetView>
-      </BottomSheetModal>
-    </View>
+        </Block>
+
+        <Block flexDirection="column" justifyContent="flex-start">
+          <IconButton
+            icon={ShareIcon}
+            iconButtonStyles={{ marginBottom: 16 }}
+          />
+          <IconButton icon={ReportIcon} />
+        </Block>
+      </Block>
+      <View style={styles.detailsContainer}>
+        <View style={styles.iconContainer}>
+          <IconButton icon={HeartIcon} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
     backgroundColor: "white",
   },
-  contentContainer: {
-    flex: 1,
-    alignItems: "center",
+
+  detailsContainer: {
+    height: "100%",
+    paddingVertical: 20,
+    paddingHorizontal: 24,
+    shadowColor: "#000000",
+    shadowOpacity: 0.08,
+    shadowOffset: {
+      width: 0,
+      height: -4,
+    },
+    backgroundColor: theme.neutral5,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    shadowRadius: 20,
   },
-  input: { height: 40, margin: 12, borderWidth: 1, padding: 10 },
+
+  iconContainer: {
+    position: "absolute",
+    top: -20,
+    right: 24,
+  },
 });
 
 export default ProductDetail;
